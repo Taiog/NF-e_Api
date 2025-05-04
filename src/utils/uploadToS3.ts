9ac3fe2c176d54dce5035329796325213ef97259
Taiog <tiagoargollo@hotmail.com>
2025-05-04 16:32:08 -0300

    feat: adds connection to aws to save xml file on s3

---
+++ src/utils/uploadToS3.ts
@@ -0,0 +1,24 @@
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+import { Readable } from 'stream';
+
+const s3 = new S3Client({
+     region: process.env.AWS_REGION,
+     credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+      },
+     });
+
+interface UploadParams {
+  Bucket: string;
+  Key: string;
+  Body: Buffer | Readable;
+  ContentType: string;
+}
+
+export async function uploadToS3(params: UploadParams): Promise<string> {
+  const command = new PutObjectCommand(params);
+  await s3.send(command);
+
+  return `https://${params.Bucket}.s3.${process.env.AWS_REGION}.amazonaws.com/${params.Key}`;
+}
